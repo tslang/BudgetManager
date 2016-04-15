f0fe95680849a76f0b273cbd1eec22774dff895b
Thad Lang <thad.lang@outlook.com>
2016-04-15 17:29:36 -0500

    Added Database Tables

---
+++ BudgetManager.Database/Tables/Account.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id]				INT					IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
+	[Name]				NVARCHAR (50)		NOT NULL,
+	[Bank]				NVARCHAR (50)		NOT NULL,
+	[AccountTypeId]		INT					NOT NULL,
+	[Amount]			MONEY				NOT NULL,
+	CONSTRAINT [FK_AccountType_Id] FOREIGN KEY ([AccountTypeId]) REFERENCES [AccountType] ([Id]), 
+    CONSTRAINT [PK_Account] PRIMARY KEY ([Id])
+)
---
+++ BudgetManager.Database/Tables/Transaction.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Transaction]
+(
+	[Id]				INT				PRIMARY KEY IDENTITY NOT NULL,
+	[Date]				DATETIME		DEFAULT (getdate()) NOT NULL,
+	[Name]				NVARCHAR(255)	NULL,
+	[Description]		NVARCHAR(255)	NOT NULL,
+	[Amount]			MONEY			NOT NULL,
+	[AccountId]			INT				NOT NULL,
+	CONSTRAINT [FK_Account_AccountId] FOREIGN KEY ([Id]) REFERENCES [Account] ([Id])
+)
