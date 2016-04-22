b266e47d578dbf2c8219a51503ce52934cd5bfec
Thad Lang <thad.lang@outlook.com>
2016-04-21 23:55:48 -0500

    Deleting edmx model and adding models from Db

---
+++ BudgetManager.Domain/BudgetManagerDbContext.cs
@@ -0,0 +1,46 @@
+namespace BudgetManager.Domain
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class BudgetManagerDbContext : DbContext
+    {
+        public BudgetManagerDbContext()
+            : base("name=BudgetManagerDbContext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<SubCategory> SubCategories { get; set; }
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Amount)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.Transactions)
+                .WithRequired(e => e.Account)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.SubCategories)
+                .WithRequired(e => e.Category)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(e => e.Transactions)
+                .WithRequired(e => e.Category)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Transaction>()
+                .Property(e => e.Amount)
+                .HasPrecision(19, 4);
+        }
+    }
+}
---
+++ BudgetManager.Domain/Models/SubCategory.cs
@@ -0,0 +1,23 @@
+namespace BudgetManager.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("SubCategory")]
+    public partial class SubCategory
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
