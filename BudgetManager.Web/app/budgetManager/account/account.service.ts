e6500f6dda0785a9876acb9e2471ab7e60f5c28d
Thad Lang <thad.lang@outlook.com>
2016-05-12 19:11:09 -0500

    Adding files from untracked

---
+++ BudgetManager.Web/app/budgetManager/account/account.service.ts
@@ -0,0 +1,40 @@
+﻿module BudgetManager.Account {
+    'use strict';
+
+    export interface IAccountService {
+        getAll(): angular.IHttpPromise<any>;
+        getDetail(id: number): angular.IHttpPromise<any>;
+        add(accountAddCommandModel: any): angular.IHttpPromise<any>;
+        update(accountUpdateCommandModel: any): angular.IHttpPromise<any>;
+    }
+
+    export class AccountService implements IAccountService {
+        public static $inject = ['dataServiceHelper'];
+
+        constructor(private dataServiceHelper: Core.IDataServiceHelper) {
+        }
+
+        public getAll(): angular.IHttpPromise<any> {
+            return this.dataServiceHelper.get(Account.AccountUrls.all);
+        }
+
+        public getDetail(id: number): angular.IHttpPromise<any> {
+            return this.dataServiceHelper.get(Account.AccountUrls.details);
+        }
+
+        public add(accountAddCommandModel: any): angular.IHttpPromise<any> {
+            return this.dataServiceHelper.postWithParameters(Account.AccountUrls.create, {
+                model: accountAddCommandModel
+            });
+        }
+
+        public update(accountUpdateCommandModel: any): angular.IHttpPromise<any> {
+            return this.dataServiceHelper.postWithParameters(Account.AccountUrls.update, {
+                model: accountUpdateCommandModel
+            });
+        }
+    }
+
+    angular.module('budgetManager.account')
+        .service('accountService', AccountService);
+}
---
+++ BudgetManager.Web/app/core/core.module.ts
@@ -0,0 +1,13 @@
+﻿module Core {
+    'use strict';
+
+    angular.module('core',
+        [
+            'ui.router',
+            'core',
+            'budgetManager.home',
+            'budgetManager.account',
+            'budgetManager.budget',
+            'budgetManager.transaction'
+        ]);
+}
